3ac3a9a5f878520e08950b7a0bc26d91f4beb6c8
untoreh <bassobassista@gmail.com>
2017-06-30 13:19:37 +0200

    - smarter checkout

--- trees.sh
+++ trees.sh
@@ -2,42 +2,48 @@
 shopt -s expand_aliases
 
 while [[ $# -gt 0 ]]; do
-		case $1 in
-				-b | --base)
-						base="$2"
-						shift
-						;;
-				-n | --name)
-						name="$2"
-						shift
-						;;
-				-f | --force)
-						clob=1
-						;;
-				-d | --delete)
-						action=delete
-						;;
-				-t | --target)
-						target="$2"
-						shift
-						;;
-				-h | --help | -* | --*)
-						action=help
-						;;
-				co | checkout)
-						action=checkout
-						;;
+	case $1 in
+		-b | --base)
+			base="$2"
+			shift
+			;;
+		-n | --name)
+			name="$2"
+			shift
+			;;
+		-f | --force)
+			clob=1
+			;;
+		-d | --delete)
+			action=delete
+			;;
+		-t | --target)
+			target="$2"
+			shift
+			;;
+		-h | --help | -* | --*)
+			action=help
+			;;
+		ls | list)
+			action=list
+			;;
+		ck | check)
+			action=check
+			;;
+		co | checkout)
+			action=checkout
+			;;
+		*)
+			case $action in
+				checkout)
+					target=${target:-$1}
+					;;
 				*)
-						case $action in
-							checkout)
-								target=${target:-$1}
-								;;
-							*)
-								name=${name:-$1}
-						esac
-						;;
-		esac
-		shift
+					name=${name:-$1}
+			esac
+			;;
+	esac
+	shift
 done
 
 ## vars
@@ -52,71 +58,84 @@ alias ostree="ostree --repo=$ostrepo"
 ## ovz
 source /etc/ovz &>/dev/null
 if [ -z "$OVZ" ]; then
-		$(which /usr/bin/ostree) admin status | grep -q ovz && \
-		echo OVZ=1 >/etc/ovz || \
-		echo OVZ=0 >/etc/ovz
+	$(which /usr/bin/ostree) admin status | grep -q ovz && \
+	echo OVZ=1 >/etc/ovz || \
+	echo OVZ=0 >/etc/ovz
 fi
 source /etc/ovz
 
 ## actions
 
 help() {
-		cat <<-EOF
-		Usage: trees APP [FLAGS]...
+	cat <<-EOF
+	Usage: trees APP [FLAGS]...
 
-		Install apps through ostree deltas checkouts.
-		-b, --base  base image (alp,trub...)
-		-n, --name  name of the app (etcd,hhvm...)
-		-f, --force clear before install
-		-d, --delete clear checkout and prune ostree repo
+	Install apps through ostree deltas checkouts.
+	-b, --base  base image (alp,trub...)
+	-n, --name  name of the app (etcd,hhvm...)
+	-f, --force clear before install
+	-d, --delete clear checkout and prune ostree repo
 
-		EOF
-		exit
+	EOF
+	exit
 }
 
 delete() {
-		[ -z "$name" ] && exit 1
-		rm -rf --one-file-system $ostrepo/$name
-		ostree refs --delete $name
-		ostree prune --refs-only
-		/usr/bin/ostree admin cleanup
+	[ -z "$name" ] && exit 1
+	rm -rf --one-file-system $ostrepo/$name
+	ostree refs --delete $name
+	ostree prune --refs-only
+	/usr/bin/ostree admin cleanup
 }
 
 install() {
-		## prepare files
-		mkdir -p $workdir && \
-				cd $workdir && \
-				\
-				[ -n "$clob" ] && \
-				rm -f $workdir/* && \
-				ostree refs --delete $name && \
-				rm -rf --one-file-system $ostrepo/$name
+	## prepare files
+	mkdir -p $workdir && \
+		cd $workdir && \
+		\
+		[ -n "$clob" ] && \
+		rm -f $workdir/* && \
+		ostree refs --delete $name && \
+		rm -rf --one-file-system $ostrepo/$name
 
-		## data
-		## ovz is only for the alpine base since they share with host repo
-		if [ $OVZ = 1 ] && [ "$base" = "pine" -o -z "$base" ]; then
-				artf="${name}_ovz.tar"
-		else
-				artf="${name}.tar"
-		fi
-		fetch_artifact ${appsrepo}:${name} $artf $workdir
-		rev=$(b64name $workdir)
-		[ -z "$rev" ] && err "error: app not found..." && exit 1
+	## data
+	## ovz is only for the alpine base since they share with host repo
+	if [ $OVZ = 1 ] && [ "$base" = "pine" -o -z "$base" ]; then
+		artf="${name}_ovz.tar"
+	else
+		artf="${name}.tar"
+	fi
+	fetch_artifact ${appsrepo}:${name} $artf $workdir
+	rev=$(b64name $workdir)
+	[ -z "$rev" ] && err "error: app not found..." && exit 1
 
-		## install
-		applied=$(ostree static-delta apply-offline $rev && echo true || echo false)
-		! $applied && err "error: troubles applying the delta..." && exit 1
-		ostree refs --create=$name $rev
+	## install
+	applied=$(ostree static-delta apply-offline $rev && echo true || echo false)
+	! $applied && err "error: troubles applying the delta..." && exit 1
+	ostree refs --create=$name $rev
 
-		## deploy
-		mkdir -p $ostrepo/$name
-		ostree checkout -H $name $ostrepo/$name/rootfs
+	ostree checkout -H $name $ostrepo/$name/rootfs
 }
 
 checkout() {
-		mkdir -p $ostrepo/$name
-		dest=${target:-$ostrepo/$name/rootfs}
-		ostree checkout --require-hardlinks --union-add $name $dest
+	## apps is available
+	check
+
+	mkdir -p $ostrepo/$name
+	dest=${target:-$ostrepo/$name/rootfs}
+	union="--union-add"
+	if [ -z "$(ostree refs $name)" ]; then
+		install
+		union=
+	fi
+	ostree checkout --require-hardlinks $union $name $dest
 }
+
+check() {
+	if [ ! -d $ostrepo ] || ! mountpoint -q $ostrepo ; then
+		printdb "linking ostree repo for apps.."
+		sup local ostree-apps
+	fi
+	}
 
 eval $action
